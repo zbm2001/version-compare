13ab533f6ab03bb74e7894221ec467e8796695a9
macbook <zbm2001@aliyun.com>
2019-06-21 16:38:40 +0800

    first version.

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * 版本比对高低
+ *
+ * @param {String} masterVersion 主版本
+ * @param {String} slaveVersion 从版本
+ * @param {Boolean} reverse 指定比对结果颠倒（小于为1；大于为-1）
+ * @return {Number} 大于为1；等于为0；小于为-1
+ */
+function index (masterVersion, slaveVersion, reverse) {
+  typeof masterVersion === 'string' && (masterVersion = masterVersion.split('.'))
+  typeof slaveVersion === 'string' && (slaveVersion = slaveVersion.split('.'))
+  let result
+  masterVersion.some((n, i) => (result = n - slaveVersion[i]))
+  return reverse ? (result < 0 ? 1 : result > 0 ? -1 : 0) : (result > 0 ? 1 : result < 0 ? -1 : 0)
+}
+
+exports = module.exports = index
+
+/**
+ * 排序升序比对函数
+ * @param {String} masterVersion
+ * @param {String} slaveVersion
+ * @returns {Number}
+ */
+exports.ascend = function ascend (masterVersion, slaveVersion) {
+  return index(masterVersion, slaveVersion, false)
+}
+
+/**
+ * 排序降序比对函数
+ * @param {String} masterVersion
+ * @param {String} slaveVersion
+ * @returns {Number}
+ */
+exports.descend = function descend (masterVersion, slaveVersion) {
+  return index(masterVersion, slaveVersion, true)
+}
